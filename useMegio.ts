d8b29a6e69bf23280521162a1c2e00cbc49b77d8
Jiří Zapletal <jz@strategio.dev>
2023-10-05 21:46:33 +0200

    Init

---
+++ useMegio.ts
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) 2023 Strategio Digital s.r.o.
+ * @author Jiří Zapletal (https://strategio.dev, jz@strategio.dev)
+ */
+
+export const useMegio = () => {
+    console.log('Welcome to Megio SDK')
+}
